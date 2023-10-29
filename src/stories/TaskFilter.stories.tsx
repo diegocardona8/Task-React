09dc3219aa3f8244693c6b6d680cb8f2165d9262
diegocardona8 <dc.soft08@gmail.com>
2023-10-29 00:57:23 -0500

    Migrate Task Filter story to CSF3

--- src/stories/TaskFilter.stories.tsx
+++ src/stories/TaskFilter.stories.tsx
@@ -1,12 +1,32 @@
+import { type } from "doctrine";
 import TaskFilter from "../components/TaskFilter";
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
-  title: "TaskFilter",
+const meta : Meta<typeof TaskFilter> = {
+  title: "Task-Filter",
   component: TaskFilter,
 };
+export default meta;
 
+type Story = StoryObj<typeof TaskFilter>;
 
-export const Default = () => <TaskFilter styleVariant="" disabled={false}/>
-export const Enabled = () => <TaskFilter styleVariant="filters-enabled" disabled={false}/>
-export const Disabled = () => <TaskFilter styleVariant="filters-disabled" disabled={false}/>
+export const Default: Story = {
+  args:{
+    styleVariant: "",
+    disabled:false,
+  }
+};
 
+export const Completed: Story = {
+  args:{
+    styleVariant: "filters-enabled",
+    disabled:false,
+  }
+};
+
+export const Pending: Story = {
+  args:{
+    styleVariant: "filters-disabled",
+    disabled:false,
+  }
+};
